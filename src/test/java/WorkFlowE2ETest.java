9e7dd82bc162a2cc793ef77d77a81160286e28c2
ArunKafene2021 <arun@kafene.com>
2022-02-22 11:13:20 -0500

    Completed the test

--- src/test/java/WorkFlowE2ETest.java
+++ src/test/java/WorkFlowE2ETest.java
@@ -1,11 +1,18 @@
 import com.up42.apireusable.TestData;
 import com.up42.apireusable.WorkFlowReusable;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 public class WorkFlowE2ETest extends BaseTest{
 
-    @Test(description = "Create workflow and add a task and monitor the job e2e scenario")
+    @Test
+    @Description("Create workflow and add a task and monitor the job e2e scenario")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("POST Request")
     public void createWorkFlowE2ETest(){
         WorkFlowReusable workFlowReusable = new WorkFlowReusable();
         Response response =  workFlowReusable.createWorkFlow(TestData.getWorkFlowData());
